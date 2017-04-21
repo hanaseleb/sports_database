877bf5f7b929e7561342f3a1601ef2613c9bd294
hanaseleb <sutoh@naturally-curly-hair.com>
2017-04-21 17:59:33 +0900

    deleat migration

--- db/migrate/20170411160155_add_userid_to_injury.rb
+++
@@ -1,5 +0,0 @@
-class AddUseridToInjury < ActiveRecord::Migration[5.0]
-  def change
-    add_column :injuries, :user_id, :string
-  end
-end
--- db/migrate/20170411225550_destroyinjury.rb
+++
@@ -1,5 +0,0 @@
-class Destroyinjury < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :injuries
-  end
-end
